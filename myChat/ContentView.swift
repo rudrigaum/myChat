51ed4c310bedaf36493716cb615575e1c2755804
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2025-03-09 08:47:16 -0300

    Initial Commit

---
+++ myChat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  myChat
+//
+//  Created by Rodrigo Cerqueira Reis on 09/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ myChat/myChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  myChatApp.swift
+//  myChat
+//
+//  Created by Rodrigo Cerqueira Reis on 09/03/25.
+//
+
+import SwiftUI
+
+@main
+struct myChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
